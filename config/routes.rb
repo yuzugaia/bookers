ab88bde75ca90c375e3c4aeb0086a3261bb46cac
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-07 07:15:39 +0000

    [add]top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get '/books' => 'books#index'
+  get 'books/show'
+  get 'books/edit'
+  get '/top' => 'homes#top'
+  
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
