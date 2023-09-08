9bc2d4acbbf5b25603a3627e95e3403993f9ce96
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-08 01:44:27 +0000

    [add]show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,15 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/show'
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -21,7 +23,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-     params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
